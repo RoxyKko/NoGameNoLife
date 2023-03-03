8e70756be3bc13e30b4df18371eef34178c0b0db
RoxyKKo <njy_roxy@outlook.com>
2023-03-03 11:42:12 +0800

    :truck:突然的变故，又不一定参加计算机设计大赛了

---
+++ Assets/Script/TitleScenesControl.cs
@@ -0,0 +1,25 @@
+using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleScenesContral : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void GameStart()
+    {
+        print("GameStart");
+        // Application.LoadLevel("GameScene1");
+    }
+}
